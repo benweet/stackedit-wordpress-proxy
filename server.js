f4b30f1ae3ae0873db9c0d2cf3ef7fb2cffc1c4a
benweet <benoit.schweblin@gmail.com>
2013-05-16 01:15:36 +0100

    First version of the application

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 var oauth2 = require('oauth').OAuth2,
     fs    = require('fs'),
+    https = require('https'),
     express = require('express');
 
 // Load config defaults from JSON file.
@@ -26,14 +27,18 @@ app.all('*', function (req, res, next) {
   next();
 });
 
-app.get('/authenticate/:code', function(req, res) {
-	console.log('authenticating code:' + req.params.code);
-	var oa = new oauth2(
+function createOauthObject() {
+	return new oauth2(
 		config.oauth_client_id,
 		config.oauth_client_secret,
 		config.oauth_base_site,
 		config.oauth_authorize_path,
 		config.oauth_access_token_path);
+}
+
+app.get('/authenticate/:code', function(req, res) {
+	console.log('authenticating code:' + req.params.code);
+	var oa = new createOauthObject();
 	oa.getOAuthAccessToken(req.params.code, {'redirect_uri': config.redirect_uri, 'grant_type': 'authorization_code'}, function(error, access_token, refresh_token, results){
 		if(error) {
 			console.log("Error: " + JSON.stringify(error));
@@ -44,6 +49,33 @@ app.get('/authenticate/:code', function(req, res) {
 		// Send OAuth token back to the client
 		res.json({token: access_token});
 	});
+});
+
+app.post('/post', function(req, res) {
+	console.log("/post");
+	//var oa = new createOauthObject();
+	var data = JSON.stringify({
+		tags: req.body.tags,
+		title: req.body.title,
+		content: req.body.content
+	});
+	var post_options = {
+		host: 'public-api.wordpress.com',
+		path: "/rest/v1/sites/" + req.body.site + "/posts/" + (req.body.postId || "new") + "?http_envelope=true",
+		method: 'POST',
+		headers: {
+			'Authorization': 'Bearer ' + req.body.token,
+			"Content-Type": "application/json",
+		}
+	};
+	// Set up the request
+	var post_req = https.request(post_options, function(response) {
+		response.pipe(res);
+	});
+
+	// post the data
+	post_req.write(data);
+	post_req.end();
 });
 
 var port = process.env.PORT || config.port || 9999;
